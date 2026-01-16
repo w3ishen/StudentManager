f00c38348c5e8a2c66d3cd382eea0778bb95eecf
dotnetdev1125 <dotnetdev1125@gmail.com>
2026-01-16 17:13:54 +0800

    Add Book list API

---
+++ aspnet-core/src/StudentManager.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManager.Books
+{
+    public class BookDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ aspnet-core/src/StudentManager.Application.Contracts/Books/GetAllBookOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManager.Books
+{
+    public class GetAllBookOutput
+    {
+        public List<BookDto> Books { get; set; }
+    }
+}
